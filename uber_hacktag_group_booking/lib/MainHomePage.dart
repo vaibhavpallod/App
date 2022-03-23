189fa188d72d86c5527111812fead35619375452
atharvasaraf123 <sarafatharva123@gmail.com>
2022-03-23 22:10:10 +0530

    Home Page on user location

--- uber_hacktag_group_booking/lib/MainHomePage.dart
+++ uber_hacktag_group_booking/lib/MainHomePage.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uber_hacktag_group_booking/konstants/loaders.dart';
+import 'package:location/location.dart';
 
 class MainHomePage extends StatefulWidget {
   @override
@@ -6,13 +11,65 @@ class MainHomePage extends StatefulWidget {
 }
 
 class _MainHomePageState extends State<MainHomePage> {
+
+
+
+  GoogleMapController _controller;
+  Location currentLocation = Location();
+  Set<Marker> _markers={};
+  bool load=true;
+  LocationData location;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+
+
+
+  void getLocation() async{
+     location= await currentLocation.getLocation();
+    currentLocation.onLocationChanged.listen((LocationData loc){
+
+      _controller?.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+        target: LatLng(loc.latitude ?? 0.0,loc.longitude?? 0.0),
+        zoom: 12.0,
+      )));
+      print(loc.latitude);
+      print(loc.longitude);
+      setState(() {
+        load=false;
+        _markers.add(Marker(markerId: MarkerId('Home'),
+            position: LatLng(loc.latitude ?? 0.0, loc.longitude ?? 0.0)
+        ));
+      });
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-
-        child: Center(child: Text('MainHomePage')),
-
+      appBar: AppBar(
+        title: Text("Map"),
+      ),
+      body: load?spinkit:Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child:GoogleMap(
+          zoomControlsEnabled: false,
+          initialCameraPosition:CameraPosition(
+            target:LatLng(location.latitude ?? 0.0,location.longitude?? 0.0),
+            zoom: 12.0,
+          ),
+          onMapCreated: (GoogleMapController controller){
+            _controller = controller;
+          },
+          markers: _markers,
+        ) ,
       ),
     );
   }
